29e803cf12cafacf6895e3469500f14feecf90b2
Laktionova <laktionova.marina@gmail.com>
2019-09-04 15:34:18 +0300

    setTimeout

--- scripts/play.js
+++ scripts/play.js
@@ -1,8 +1,23 @@
 let sumHamelion = 0
 
-let arrayGlasnie = ["А а", "О о", "Е е", "У у", "И и", "Я я", "Э э", "Ю ю", "Ы ы", "Ё ё"]
-let arraySoglasnie = ["Б", "В", "Г", "Д", "Ж", "З", "К", "Л", "М", "Н", "П", "Р", "С", "Т", "Ф", "Х", "Ц", "Ш", "Щ", "Ь", "Ъ", "Ч"]
-let arraySlogi = ["акт", "бак", "бас", "бег", "боб", "бок", "бор", "бук", "бык", "век", "вес", "вид", "вол", "вор", "все"]
+let arrayGlasnie = []
+
+data.sounds.forEach(sound => {
+    if (sound.id < 11) {
+        arrayGlasnie.push(sound)
+    }
+})
+console.log(arrayGlasnie)
+
+let arraySoglasnie = []
+data.sounds.forEach(sound => {
+    if (sound.id > 10) {
+        arraySoglasnie.push(sound)
+    }
+})
+console.log(arraySoglasnie)
+
+let arraySlova = ["акт", "бак", "бас", "бег", "боб", "бок", "бор", "бук", "бык", "век", "вес", "вид", "вол", "вор", "все"]
 let arrayLetters = []
 let arrayColors = ["red", "green", "blue", "brown", "white", "yellow", "pink", "violet", "orange", "gray"]
 
@@ -18,24 +33,35 @@ function create(tag) {
 playbtn.onclick = () => {
     playbtn.remove()
     header.remove()
+    msg = new SpeechSynthesisUtterance(`Найди букву или слово!`)
+    msg.lang = "ru"
+    msg.pitch = 0.2
+    speechSynthesis.speak(msg)
+    // var mydata = data.sounds
+    // console.log(mydata)
+    setTimeout(() => {
     let newSheet = () => {
         console.log(sumHamelion)
         if (sumHamelion < 3) {
             arrayLetters = arrayGlasnie
         }
-        else if (sumHamelion > 2 && sumHamelion < 6) {
+        else if (sumHamelion > 2 && sumHamelion < 16) {
             arrayLetters = arraySoglasnie
         }
-        else {
-            arrayLetters = arraySlogi
-        }
+        /*       else {
+                   arrayLetters = arraySlova
+               }*/
+        
         class Letter {
             constructor(top, left) {
                 this.letter = this.createLetter(top, left)
             }
+            
             createLetter(top, left) {
                 let letterRand = create('div')
-                letterRand.innerText = arrayLetters[rand(0, arrayLetters.length - 1)]
+                let rLetter = arrayLetters[rand(0, arrayLetters.length - 1)]
+                letterRand.innerText = rLetter.name
+                letterRand.href = rLetter.value
                 letterRand.style.color = arrayColors[rand(0, arrayColors.length - 1)]
                 letterRand.style.position = "absolute"
                 letterRand.style.top = `${Number(top)}%`
@@ -54,14 +80,15 @@ playbtn.onclick = () => {
         let letterForQuestion = arrayForQuestion[rand(0, arrayForQuestion.length - 1)]
 
         let msg = ""
-        if (letterForQuestion.letter.innerText.length === 1) {
-            msg = new SpeechSynthesisUtterance(`Найди букву ${letterForQuestion.letter.innerText}`)
+        if (letterForQuestion.letter.href.indexOf("s") !== -1) {
+            var audio = new Audio(letterForQuestion.letter.href)
+            audio.play()
         }
         else {
-            msg = new SpeechSynthesisUtterance(`Найди слово ${letterForQuestion.letter.innerText}`)
+            msg = new SpeechSynthesisUtterance(`${letterForQuestion.letter.innerText}`)
+            msg.lang = "ru"
+            speechSynthesis.speak(msg)
         }
-        msg.lang = "ru"
-        speechSynthesis.speak(msg)
 
         arrayForQuestion.forEach(element => {
             if (String(element.letter.innerText) == String(letterForQuestion.letter.innerText)) {
@@ -87,4 +114,5 @@ playbtn.onclick = () => {
         window.location.href = "index.html"
 
     }, 60000)
+}, 2000);
 }
