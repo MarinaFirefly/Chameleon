8b4c47aefa2aeda18074983bc3d474ca210045ad
Marina <51212211+MarinaFirefly@users.noreply.github.com>
2020-03-03 13:56:24 +0200

    Add files via upload

---
+++ scripts/db.js
@@ -0,0 +1,169 @@
+let data = {
+    "sounds": [
+        {
+            "id": 1,
+            "name": "А а",
+            "value": "sounds/а.mp3"
+        },
+        {
+            "id": 2,
+            "name": "О о",
+            "value": "sounds/о.mp3"
+        },
+        {
+            "id": 3,
+            "name": "Е е",
+            "value": "sounds/е.mp3"
+        },
+        {
+            "id": 4,
+            "name": "И и",
+            "value": "sounds/и.mp3"
+        },
+        {
+            "id": 5,
+            "name": "У у",
+            "value": "sounds/у.mp3"
+        },
+        {
+            "id": 6,
+            "name": "Я я",
+            "value": "sounds/я.mp3"
+        },
+        {
+            "id": 7,
+            "name": "ы",
+            "value": "sounds/ы.mp3"
+        },
+        {
+            "id": 8,
+            "name": "Ю ю",
+            "value": "sounds/ю.mp3"
+        },
+        {
+            "id": 9,
+            "name": "Э э",
+            "value": "sounds/э.mp3"
+        },
+        {
+            "id": 10,
+            "name": "Ё ё",
+            "value": "sounds/ё.mp3"
+        },
+        {
+            "id": 11,
+            "name": "Б б",
+            "value": "sounds/б.mp3"
+        },
+        {
+            "id": 12,
+            "name": "В в",
+            "value": "sounds/в.mp3"
+        },
+        {
+            "id": 13,
+            "name": "Г г",
+            "value": "sounds/г.mp3"
+        },
+        {
+            "id": 14,
+            "name": "Д д",
+            "value": "sounds/д.mp3"
+        },
+        {
+            "id": 15,
+            "name": "Ж ж",
+            "value": "sounds/ж.mp3"
+        },
+        {
+            "id": 16,
+            "name": "З з",
+            "value": "sounds/з.mp3"
+        },
+        {
+            "id": 17,
+            "name": "К к",
+            "value": "sounds/к.mp3"
+        },
+        {
+            "id": 18,
+            "name": "Л л",
+            "value": "sounds/л.mp3"
+        },
+        {
+            "id": 19,
+            "name": "М м",
+            "value": "sounds/м.mp3"
+        },
+        {
+            "id": 20,
+            "name": "Н н",
+            "value": "sounds/н.mp3"
+        },
+        {
+            "id": 21,
+            "name": "П п",
+            "value": "sounds/п.mp3"
+        },
+        {
+            "id": 22,
+            "name": "Р р",
+            "value": "sounds/р.mp3"
+        },
+        {
+            "id": 23,
+            "name": "С с",
+            "value": "sounds/с.mp3"
+        },
+        {
+            "id": 24,
+            "name": "Т т",
+            "value": "sounds/т.mp3"
+        },
+        {
+            "id": 25,
+            "name": "Ф ф",
+            "value": "sounds/ф.mp3"
+        },
+        {
+            "id": 26,
+            "name": "Х х",
+            "value": "sounds/х.mp3"
+        },
+        {
+            "id": 27,
+            "name": "Ц ц",
+            "value": "sounds/ц.mp3"
+        },
+        {
+            "id": 28,
+            "name": "Ч ч",
+            "value": "sounds/ч.mp3"
+        },
+        {
+            "id": 29,
+            "name": "ъ",
+            "value": "sounds/ъ.mp3"
+        },
+        {
+            "id": 30,
+            "name": "ь",
+            "value": "sounds/ь.mp3"
+        },
+        {
+            "id": 31,
+            "name": "Ш ш",
+            "value": "sounds/ш.mp3"
+        },
+        {
+            "id": 32,
+            "name": "Щ щ",
+            "value": "sounds/щ.mp3"
+        },
+        {
+            "id": 33,
+            "name": "Й й",
+            "value": "sounds/й.mp3"
+        }
+    ]
+}
--- scripts/play.js
+++ scripts/play.js
@@ -1,8 +1,19 @@
 let sumHamelion = 0
 
-let arrayGlasnie = ["А", "О", "Е", "У", "И", "Я", "Э", "Ю", "Ы", "Ё"]
-let arraySoglasnie = ["Б", "В", "Г", "Д", "Ж", "З", "К", "Л", "М", "Н", "П", "Р", "С", "Т", "Ф", "Х", "Ц", "Ш", "Щ", "Ь", "Ъ", "Ч"]
-let arraySlogi = ["акт", "бак", "бас", "бег", "боб", "бок", "бор", "бук", "бык", "век", "вес", "вид", "вол", "вор", "все"]
+let arrayGlasnie = []
+data.sounds.forEach(sound => {
+    if (sound.id < 11) {
+        arrayGlasnie.push(sound)
+    }
+})
+
+let arraySoglasnie = []
+data.sounds.forEach(sound => {
+    if (sound.id > 10) {
+        arraySoglasnie.push(sound)
+    }
+})
+
 let arrayLetters = []
 let arrayColors = ["red", "green", "blue", "brown", "white", "yellow", "pink", "violet", "orange", "gray"]
 
@@ -14,77 +25,164 @@ function create(tag) {
     return document.createElement(tag)
 }
 
+class Bug {
+    constructor(image, size) {
+        this.bug = this.create(image, size)
+    }
+
+    create(image, size) {
+        var img = document.createElement("img")
+        img.className = "bugs"
+        img.src = image
+        img.width = size
+        img.style.position = 'absolute'
+        img.style.top = `${rand(0, window.innerHeight - 55)}px`
+        img.style.left = `${rand(0, window.innerWidth - 55)}px`
+        return img
+    }
+    appendTo(divname) {
+        divname.appendChild(this.bug)
+    }
+
+    flex() {
+        let bug = this.bug
+        function flexingBug() {
+            bug.style.top = `${rand(0, (window.innerHeight - 55))}px`
+            bug.style.left = `${rand(0, (window.innerWidth - 55))}px`
+            bug.style.transition = '5s'
+            setTimeout(flexingBug, rand(1000, 5000))
+        }
+        flexingBug()
+    }
+    rotation() {
+        let bug = this.bug
+        function bugRotate() {
+            bug.style.transform = `rotate(${rand(90, 270)}deg)`
+            bug.style.transition = '0.1s'
+            setTimeout(bugRotate, 5000)
+        }
+        bugRotate()
+    }
+}
 
 playbtn.onclick = () => {
     playbtn.remove()
     header.remove()
-    let newSheet = () => {
-        console.log(sumHamelion)
-        if (sumHamelion < 3) {
-            arrayLetters = arrayGlasnie
-        }
-        else if (sumHamelion > 2 && sumHamelion < 6) {
-            arrayLetters = arraySoglasnie
-        }
-        else {
-            arrayLetters = arraySlogi
-        }
-        class Letter {
-            constructor(top, left) {
-                this.letter = this.createLetter(top, left)
+    msg = new SpeechSynthesisUtterance(`Найди букву!`)
+    msg.lang = "ru"
+    msg.pitch = 0.3
+    speechSynthesis.speak(msg)
+    setTimeout(() => {
+        let newSheet = () => {
+            console.log(sumHamelion)
+            if (sumHamelion < 6) {
+                arrayLetters = arrayGlasnie
             }
-            createLetter(top, left) {
-                let letterRand = create('div')
-                letterRand.innerText = arrayLetters[rand(0, arrayLetters.length - 1)]
-                letterRand.style.color = arrayColors[rand(0, arrayColors.length - 1)]
-                letterRand.style.position = "absolute"
-                letterRand.style.top = `${Number(top)}%`
-                letterRand.style.fontSize = "250px"
-                letterRand.style.left = `${Number(left)}%`
-                letterdiv.appendChild(letterRand)
-                return this.letter = letterRand
+            else {
+                arrayLetters = arraySoglasnie
             }
-        }
-        let first = new Letter("10", "15")
-        let second = new Letter("10", "65")
-        let third = new Letter("50", "15")
-        let fourth = new Letter("50", "65")
 
-        let arrayForQuestion = [first, second, third, fourth]
-        let letterForQuestion = arrayForQuestion[rand(0, arrayForQuestion.length - 1)]
+            class Letter {
+                constructor(top, left) {
+                    this.letter = this.createLetter(top, left)
+                }
 
-        let msg = ""
-        if (letterForQuestion.letter.innerText.length === 1) {
-            msg = new SpeechSynthesisUtterance(`Найди букву ${letterForQuestion.letter.innerText}`)
-        }
-        else {
-            msg = new SpeechSynthesisUtterance(`Найди слово ${letterForQuestion.letter.innerText}`)
-        }
-        msg.lang = "ru"
-        speechSynthesis.speak(msg)
-
-        arrayForQuestion.forEach(element => {
-            if (String(element.letter.innerText) == String(letterForQuestion.letter.innerText)) {
-                element.letter.onclick = () => {
-                    ++sumHamelion
-                    letterdiv.innerText = ""
-                    let ham = create("img")
-                    ham.src = "images/hamelion1.gif"
-                    ham.id = "chameleon"
-                    document.body.appendChild(ham)
-                    setTimeout(() => {
-                        ham.remove()
-                        newSheet()
-                    }, 1500)
-
+                createLetter(top, left) {
+                    let letterRand = create('div')
+                    let rLetter = arrayLetters[rand(0, arrayLetters.length - 1)]
+                    letterRand.innerText = rLetter.name
+                    letterRand.href = rLetter.value
+                    letterRand.style = `position:absolute; width: 50%; text-align: center; color: ${arrayColors[rand(0, arrayColors.length - 1)]}`
+                    letterRand.style.top = `${Number(top)}%`
+                    letterRand.style.fontSize = "200px"
+                    letterRand.style.left = `${Number(left)}%`
+                    letterdiv.appendChild(letterRand)
+                    return this.letter = letterRand
                 }
             }
-        })
-    }
-    newSheet()
-    setTimeout(() => {
-        alert("Сумма очков " + sumHamelion)
-        window.location.href = "index.html"
+            let first = new Letter("10", "0")
+            let second = new Letter("10", "50")
+            let third = new Letter("50", "0")
+            let fourth = new Letter("50", "50")
 
-    }, 60000)
+            let arrayForQuestion = [first, second, third, fourth]
+            let letterForQuestion = arrayForQuestion[rand(0, arrayForQuestion.length - 1)]
+
+            let msg = ""
+            if (letterForQuestion.letter.href.indexOf("s") !== -1) {
+                var audio = new Audio(letterForQuestion.letter.href)
+                audio.play()
+            }
+            else {
+                msg = new SpeechSynthesisUtterance(`${letterForQuestion.letter.innerText}`)
+                msg.lang = "ru"
+                speechSynthesis.speak(msg)
+            }
+
+            arrayForQuestion.forEach(element => {
+                if (String(element.letter.innerText) == String(letterForQuestion.letter.innerText)) {
+                    element.letter.onclick = () => {
+                        ++sumHamelion
+                        letterdiv.innerText = ""
+                        let ham = create("img")
+                        ham.src = "images/chameleon1.gif"
+                        ham.id = "chameleon"
+                        document.body.appendChild(ham)
+                        setTimeout(() => {
+                            ham.remove()
+                            newSheet()
+                        }, 1500)
+
+                    }
+                }
+                else {
+                    element.letter.onclick = () => {
+                        letterdiv.innerText = ""
+                        let smile = create("img")
+                        smile.src = "images/sad1.png"
+                        smile.id = "sad"
+                        document.body.appendChild(smile)
+                        setTimeout(() => {
+                            smile.remove()
+                            newSheet()
+                        }, 1000)
+
+                    }
+                }
+            })
+        }
+        newSheet()
+        setTimeout(() => {
+            let result = async () => {
+                let bugTxt = ``
+                for (let i = 0; i < sumHamelion; i++) {
+                    bugTxt += `<i class="fas fa-bug"></i>`
+                }
+                document.body.innerHTML = `<div id="modal">\n<div id="modalHeader">\n<button id="btnTimes">
+            \n<i class="fas fa-times fa-2x" style="color: white"></i>\n</button>\n</div>\n
+            <div id="modalTxt">Результат ${sumHamelion} \n ${bugTxt}</div>\n</div>\n<div id="modalBack">\n
+            <img id="chamBack" src="images/chameleon3.png">\n</div>\n
+            <footer class="footer">\n<p class="text-in-block">Chameleon by Laktionova</p>\n</footer>`
+            }
+            result().then(element => {
+                for (let i = 0; i < sumHamelion; i++) {
+                    let bug = new Bug(`images/bug${rand(1, 6)}.png`, `${rand(70, 110)}`)
+                    bug.appendTo(modalBack)
+                    bug.rotation()
+                    bug.flex()
+                }
+                btnTimes.onclick = () => window.location.href = "index.html"
+            }).then(element => {
+                setTimeout(function delBug() {
+                    let bugDel = document.getElementsByClassName("bugs")[0]
+                    bugDel.parentNode.removeChild(bugDel)
+                    let i = 0
+                    if (i < document.getElementsByClassName("bugs").length) {
+                        setTimeout(() => delBug(), 5000)
+                        i++
+                    }
+                }, 5000)
+            })
+        }, 60000)
+    }, 2000)
 }
